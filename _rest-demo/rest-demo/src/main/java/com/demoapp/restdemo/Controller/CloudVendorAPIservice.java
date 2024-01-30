035422cb589228e1a3e80a8aa47011055cf208da
Fai-chebby <chebetf034@gmail.com>
2024-01-30 13:36:47 +0300

    fun

--- _rest-demo/rest-demo/src/main/java/com/demoapp/restdemo/Controller/CloudVendorAPIservice.java
+++ _rest-demo/rest-demo/src/main/java/com/demoapp/restdemo/Controller/CloudVendorAPIservice.java
@@ -1,6 +1,8 @@
 package com.demoapp.restdemo.Controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,21 +18,28 @@ public class CloudVendorAPIservice {
 	
 	CloudVendor cloudVendor;
 	@GetMapping("{vendorId}")
-	public CloudVendor getCloudvendorDetails(String vendorId) {
+	public CloudVendor getCloudvendorDetails( @PathVariable String vendorId) {
 	return  cloudVendor;
 	 //(  "c1", "vendor 1", "Address one","xxxx");	
 	}
 	
 	@PostMapping
-	public String createCloudVendorDetails ( @RequestBody CloudVendor cloudvendor) {
-		this.cloudVendor =cloudVendor;
-		return " cloud vendor created successfuly";
+	public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+	    this.cloudVendor = cloudVendor;
+	    return "Cloud vendor created successfully";
 	}
+
 	@PutMapping
-	public String updateCloudVendorDetails ( @RequestBody CloudVendor cloudvendor) {
-		this.cloudVendor =cloudVendor;
-		return " cloud vendor updated successfuly";
-	}	
+	public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+	    this.cloudVendor = cloudVendor;
+	    return "Cloud vendor updated successfully";
+	}
+
+	@DeleteMapping("{vendorId}")
+	public String deleteCloudVendorDetails ( String vendorId) {
+		this.cloudVendor =null;
+		return " cloud vendor Deleted successfuly";
+	}
 
 }
  
