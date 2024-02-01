8b0500495f3aa342fc6535d7e1b3c2a02c64c396
Fai-chebby <chebetf034@gmail.com>
2024-02-01 23:23:54 +0300

    few basics

---
+++ demoPraco/src/main/java/demo/example/demopraco/DemoPracoApplication.java
@@ -0,0 +1,13 @@
+package demo.example.demopraco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPracoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPracoApplication.class, args);
+	}
+
+}
