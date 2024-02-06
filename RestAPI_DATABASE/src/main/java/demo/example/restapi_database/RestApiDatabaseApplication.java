1291f1e975c66bc80a2957b55b654f8b71629737
Fai-chebby <chebetf034@gmail.com>
2024-02-06 11:32:32 +0300

    Spring boot

--- RestAPI_DATABASE/src/main/java/demo/example/restapi_database/RestApiDatabaseApplication.java
+++ RestAPI_DATABASE/src/main/java/demo/example/restapi_database/RestApiDatabaseApplication.java
@@ -2,12 +2,22 @@ package demo.example.restapi_database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class RestApiDatabaseApplication {
+@RestController
+public class  RestApiDatabaseApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(RestApiDatabaseApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello(){
+        return "Hello world";
+
     }
 
 }
