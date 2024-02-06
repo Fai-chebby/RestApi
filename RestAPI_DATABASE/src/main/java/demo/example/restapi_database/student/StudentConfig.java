1f865a5512efa1ad905d56021bd0f8e2e760c283
Fai-chebby <chebetf034@gmail.com>
2024-02-06 13:02:39 +0300

    Spring Jpa

---
+++ RestAPI_DATABASE/src/main/java/demo/example/restapi_database/student/StudentConfig.java
@@ -0,0 +1,42 @@
+package demo.example.restapi_database.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+   CommandLineRunner commandLineRunner(
+           StudentRepository repository
+   ){
+       return args -> {
+          Student FAITH= new Student(
+
+                   1L,
+                   "FAITH",
+                   "CHEBETF034@GMAIL",
+                   LocalDate.of(2002, Month.JANUARY,4),
+                   21
+
+
+           );
+           Student ROP= new Student(
+                   "ROP",
+                   "ROPF034@GMAIL",
+                   LocalDate.of(2005, Month.JANUARY,4),
+                   21
+
+
+           );
+           repository.saveAll(List.of(FAITH,ROP));
+
+
+
+       };
+   }
+}
