b2c9dc20ba80581c266fff2c49a8346b7a551ff7
Fai-chebby <chebetf034@gmail.com>
2024-02-06 11:55:28 +0300

    spring API

--- RestAPI_DATABASE/src/main/java/demo/example/restapi_database/RestApiDatabaseApplication.java
+++ RestAPI_DATABASE/src/main/java/demo/example/restapi_database/RestApiDatabaseApplication.java
@@ -1,9 +1,14 @@
 package demo.example.restapi_database;
 
+import demo.example.restapi_database.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -15,8 +20,17 @@ public class  RestApiDatabaseApplication {
     }
 
     @GetMapping
-    public String hello(){
-        return "Hello world";
+    public List<Student> hello(){
+        return  List.of(
+                new Student(
+                       1L,
+                        "FAITH",
+                        "CHEBETF034@GMAIL",
+                        LocalDate.of( 2000, Month.JANUARY, 5),
+                        21
+
+                )
+        );
 
     }
 
