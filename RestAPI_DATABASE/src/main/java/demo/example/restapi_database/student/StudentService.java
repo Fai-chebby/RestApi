c86107a3f9a39582316844da2d8617727b996ae9
Fai-chebby <chebetf034@gmail.com>
2024-02-06 12:44:50 +0300

    Spring Jpa

--- RestAPI_DATABASE/src/main/java/demo/example/restapi_database/student/StudentService.java
+++ RestAPI_DATABASE/src/main/java/demo/example/restapi_database/student/StudentService.java
@@ -1,5 +1,6 @@
 package demo.example.restapi_database.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,16 +10,12 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+    @Autowired
+    public  StudentService(StudentRepository studentRepository){
+        this.studentRepository=studentRepository;
+    }
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "FAITH",
-                        "CHEBETF034@GMAIL",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-
-                )
-        );
+        return studentRepository.findAll();
     }
 }
