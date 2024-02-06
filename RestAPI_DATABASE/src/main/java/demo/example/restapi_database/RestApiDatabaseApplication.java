dcfd65d017292013e6c23913a7fc307f1e921805
Fai-chebby <chebetf034@gmail.com>
2024-02-06 11:19:38 +0300

    Spring Boot

---
+++ RestAPI_DATABASE/src/main/java/demo/example/restapi_database/RestApiDatabaseApplication.java
@@ -0,0 +1,13 @@
+package demo.example.restapi_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiDatabaseApplication.class, args);
+    }
+
+}
---
+++ RestAPI_DATABASE/src/test/java/demo/example/restapi_database/RestApiDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.example.restapi_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
