999101cd23f6178b98bc082d6af9baae46dd0b10
Fai-chebby <chebetf034@gmail.com>
2024-02-08 12:40:23 +0300

    login basics

--- loginForm/src/main/java/demo/example/loginform/appuser/AppUserService.java
+++ loginForm/src/main/java/demo/example/loginform/appuser/AppUserService.java
@@ -9,10 +9,12 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 @Service
 public class AppUserService implements UserDetailsService {
+    private final static  String USER_NOT_FOUND_MSG="user with email %s not found";
     private final AppUserRepository appUserRepository;
     @Override
-    public UserDetails loadUserByUsername(String username)
+    public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
-        return appUserRepository.findByEmail(email);
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG)));
     }
 }
