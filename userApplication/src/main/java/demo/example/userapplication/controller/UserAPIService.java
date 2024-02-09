0ec39f0cb25babc8139a0636269abaa37ba88392
Fai-chebby <chebetf034@gmail.com>
2024-02-09 10:36:37 +0300

    userApplication

--- userApplication/src/main/java/demo/example/userapplication/controller/UserAPIService.java
+++ userApplication/src/main/java/demo/example/userapplication/controller/UserAPIService.java
@@ -2,15 +2,16 @@ package demo.example.userapplication.controller;
 
 import demo.example.userapplication.User.user;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("user")
 public class UserAPIService {
-    @GetMapping({"userid"})
-    public user getUserDetails( String userid){
+    @GetMapping("/{userid}") // Use curly braces for path variable
+    public user getUserDetails(@PathVariable String userid){ // Add @PathVariable annotation
 
-        return new user(" C1 ","User1","Address 1","567");
+        return new user("C1", "User1", "Address 1", "567");
     }
 }
