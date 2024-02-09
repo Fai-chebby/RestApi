48234a8e36f65870b0a8905e71a5eac2de7594f8
Fai-chebby <chebetf034@gmail.com>
2024-02-09 09:48:35 +0300

    userApplication

---
+++ userApplication/src/main/java/demo/example/userapplication/User/user.java
@@ -0,0 +1,17 @@
+package demo.example.userapplication.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class user {
+    private String userId;
+    private  String userNames;
+    private  String userAddress;
+    private String userPhoneNumber;
+}
