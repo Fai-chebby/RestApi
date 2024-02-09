ba825ca34fd8e9e348817f5eeea01706aa0c1978
Fai-chebby <chebetf034@gmail.com>
2024-02-09 16:34:44 +0300

    userApplication

--- userApplication/src/main/java/demo/example/userapplication/Exception/UserExceptionHandler.java
+++ userApplication/src/main/java/demo/example/userapplication/Exception/UserExceptionHandler.java
@@ -2,8 +2,12 @@ package demo.example.userapplication.Exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class UserExceptionHandler {
+    @ExceptionHandler(value = {UserNotFoundException.class})
     public ResponseEntity<Object> handleUserNotFoundException
             (UserNotFoundException  userNotFoundException){
         UserException userException =new UserException(
