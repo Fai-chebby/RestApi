35d1992b42e1102dec80221b2418e4397e5833db
Fai-chebby <chebetf034@gmail.com>
2024-02-09 17:04:32 +0300

    userApplication

--- userApplication/src/main/java/demo/example/userapplication/response/responseHandler.java
+++ userApplication/src/main/java/demo/example/userapplication/response/responseHandler.java
@@ -1,2 +1,19 @@
-package demo.example.userapplication.response;public class responseHandler {
+package demo.example.userapplication.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class responseHandler {
+    public  static ResponseEntity<Object> responseBuilder(
+            String message, HttpStatus httpStatus, Object responseObject
+            ){
+        Map<String,Object> response =new HashMap<>();
+        response.put("message",message);
+        response.put("httpStatus",httpStatus);
+        response.put("data",responseObject);
+    }
+
 }
