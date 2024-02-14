67b9308c3b8705b886777f46f6566b8a2694658d
Fai-chebby <chebetf034@gmail.com>
2024-02-14 23:53:23 +0300

    userApplication

--- userApplication/src/main/java/demo/example/userapplication/repository/UserRepository.java
+++ userApplication/src/main/java/demo/example/userapplication/repository/UserRepository.java
@@ -3,5 +3,8 @@ package demo.example.userapplication.repository;
 import demo.example.userapplication.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository <User, String>{
+    List<User>findByUserNames (String userNames);
 }
---
+++ userApplication/src/test/java/demo/example/userapplication/repository/userRepositoryTest.java
@@ -0,0 +1,4 @@
+package demo.example.userapplication.repository;
+
+public class userRepositoryTest {
+}
