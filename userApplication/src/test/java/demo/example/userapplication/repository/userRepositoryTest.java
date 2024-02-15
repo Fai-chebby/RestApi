b3dc18c12c082db2db9cb9354d0b244adb52974e
Fai-chebby <chebetf034@gmail.com>
2024-02-15 13:46:39 +0300

    userApplication

--- userApplication/src/test/java/demo/example/userapplication/repository/userRepositoryTest.java
+++ userApplication/src/test/java/demo/example/userapplication/repository/userRepositoryTest.java
@@ -1,6 +1,8 @@
 package demo.example.userapplication.repository;
 
 import demo.example.userapplication.User.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -9,4 +11,15 @@ public class userRepositoryTest {
     @Autowired
     private UserRepository userRepository;
     User user;
+
+    @BeforeEach
+    void setUp() {
+        user= new User("1","Rop","uas","1234");
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
 }
