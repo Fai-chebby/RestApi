d5a02bc9dfbd058e96f8bc028b52a6e87a0128b8
Fai-chebby <chebetf034@gmail.com>
2024-02-15 13:40:44 +0300

    userApplication

--- userApplication/src/test/java/demo/example/userapplication/repository/userRepositoryTest.java
+++ userApplication/src/test/java/demo/example/userapplication/repository/userRepositoryTest.java
@@ -1,4 +1,12 @@
 package demo.example.userapplication.repository;
 
+import demo.example.userapplication.User.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class userRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+    User user;
 }
