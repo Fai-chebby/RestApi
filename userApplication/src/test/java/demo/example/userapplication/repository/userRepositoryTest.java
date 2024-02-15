e62382b419f2d65bc01011fccc130bb8b37d53d3
Fai-chebby <chebetf034@gmail.com>
2024-02-15 13:57:59 +0300

    userApplication

--- userApplication/src/test/java/demo/example/userapplication/repository/userRepositoryTest.java
+++ userApplication/src/test/java/demo/example/userapplication/repository/userRepositoryTest.java
@@ -3,8 +3,13 @@ package demo.example.userapplication.repository;
 import demo.example.userapplication.User.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 public class userRepositoryTest {
@@ -26,4 +31,13 @@ public class userRepositoryTest {
 
     }
     // test case success
+    @Test
+    void testFindByVendorName_Found()
+    {
+       List<User>userList=  userRepository.findByUserNames("Rop");
+       assertThat(userList.get(0).getUserId()).isEqualTo(user.getUserId());
+       assertThat(userList.get(0).getUserAddress()).isEqualTo(user.getUserAddress());
+       
+    }
+    // Test case fail
 }
