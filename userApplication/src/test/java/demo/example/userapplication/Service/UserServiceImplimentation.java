a8c7e9e0a9be704d487d9f1e535267445efd33f7
Fai-chebby <chebetf034@gmail.com>
2024-02-15 14:34:05 +0300

    userApplication

---
+++ userApplication/src/test/java/demo/example/userapplication/Service/UserServiceImplimentation.java
@@ -0,0 +1,8 @@
+package demo.example.userapplication.Service;
+
+import demo.example.userapplication.repository.UserRepository;
+
+public class UserServiceImplimentation implements UserService {
+    public UserServiceImplimentation(UserRepository userRepository) {
+    }
+}
--- userApplication/src/test/java/demo/example/userapplication/Service/userImplementationTestS.java
+++ userApplication/src/test/java/demo/example/userapplication/Service/userImplementationTestS.java
@@ -1,10 +1,13 @@
 package demo.example.userapplication.Service;
 
+import demo.example.userapplication.User.User;
 import demo.example.userapplication.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,10 +16,13 @@ class userImplementationTestS {
     private UserRepository userRepository;
     private UserService userService;
     AutoCloseable autoCloseable;
-    U
+    User user;
 
     @BeforeEach
     void setUp() {
+        autoCloseable= MockitoAnnotations.openMocks(this);
+        userService =new UserServiceImplimentation(userRepository);
+        user=new User("1","ROP","USA","12330");
     }
 
     @AfterEach
