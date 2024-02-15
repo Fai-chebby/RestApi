ffe8ae6c9e7782312b60d87d7bd62761e3656893
Fai-chebby <chebetf034@gmail.com>
2024-02-15 14:24:57 +0300

    userApplication

---
+++ userApplication/src/test/java/demo/example/userapplication/Service/userImplementationTestS.java
@@ -0,0 +1,43 @@
+package demo.example.userapplication.Service;
+
+import demo.example.userapplication.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class userImplementationTestS {
+    @Mock
+    private UserRepository userRepository;
+    
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+}
