5527374c33055b66ab2cad95d57dc18e75f580b1
Fai-chebby <chebetf034@gmail.com>
2024-04-01 22:41:32 +0300

    LaundryModel

--- laundry/src/main/java/model/LaundryModel.java
+++ laundry/src/main/java/model/LaundryModel.java
@@ -1,7 +1,20 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LaundryModel {
-    private String name;
-    
+    private String Id;
+    private String Name;
+    private String Email;
+    private String Location;
+    private int phone_no;
+
 
 }
