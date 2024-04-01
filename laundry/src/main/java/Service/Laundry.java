4b36947124b6bed45547e947f6d817ac64c79d1c
Fai-chebby <chebetf034@gmail.com>
2024-04-01 23:02:05 +0300

    Laundry

--- laundry/src/main/java/Service/Laundry.java
+++ laundry/src/main/java/Service/Laundry.java
@@ -1,5 +1,19 @@
 package Service;
 
+import demo.example.laundry.LaundryApplication;
+
+import java.util.List;
+
 public interface Laundry {
-    
+    public String createUser(Laundry laundry);
+
+    public String updateUser(Laundry laundry);
+
+    public String deleteUser(String laundryId);
+
+    public Laundry getUser(String laundryId);
+
+    public default List<Laundry> getAllUsers() {
+        return null;
+    }
 }
--- laundry/src/main/java/Service/LaundryService.java
+++ laundry/src/main/java/Service/LaundryService.java
@@ -1,4 +1,30 @@
 package Service;
 
-public class LaundryService {
+import java.util.List;
+
+public class LaundryService implements Laundry{
+    @Override
+    public String createUser(Laundry laundry) {
+        return null;
+    }
+
+    @Override
+    public String updateUser(Laundry laundry) {
+        return null;
+    }
+
+    @Override
+    public String deleteUser(String laundryId) {
+        return null;
+    }
+
+    @Override
+    public Laundry getUser(String laundryId) {
+        return null;
+    }
+
+    @Override
+    public List<Laundry> getAllUsers() {
+        return Laundry.super.getAllUsers();
+    }
 }
