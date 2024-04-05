4bc3a12fc57a45a6286fe6f0534ae0e706733bc6
Fai-chebby <chebetf034@gmail.com>
2024-04-05 22:17:03 +0300

    laundry

--- laundry/src/main/java/Exception/LaundryException.java
+++ laundry/src/main/java/Exception/LaundryException.java
@@ -1,8 +1,12 @@
 package Exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 @AllArgsConstructor
+@Getter
+@Setter
 public class LaundryException {
     private final String Message;
     private final Throwable throwable;
