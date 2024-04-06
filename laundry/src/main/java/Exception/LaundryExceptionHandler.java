ecdf0aaf9f4d447ed0c1a1084cd3d29137716208
Fai-chebby <chebetf034@gmail.com>
2024-04-06 22:33:13 +0300

    laundryApplication

--- laundry/src/main/java/Exception/LaundryExceptionHandler.java
+++ laundry/src/main/java/Exception/LaundryExceptionHandler.java
@@ -2,8 +2,11 @@ package Exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 public class LaundryExceptionHandler {
+
+    @ExceptionHandler(LaundryApplicationNotFoundException.class)
     public ResponseEntity<Object> handleLaundryApplicationNotFoundException
             (LaundryApplicationNotFoundException laundryApplicationNotFoundException)
     {
@@ -12,5 +15,6 @@ public class LaundryExceptionHandler {
                 laundryApplicationNotFoundException.getCause(),
                 HttpStatus.NOT_FOUND
         );
+        return new ResponseEntity<>(laundryException ,HttpStatus.NOT_FOUND);
     }
 }
