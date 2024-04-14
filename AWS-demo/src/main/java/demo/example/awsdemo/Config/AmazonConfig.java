ee2faab3bff0efba7b56c23a44522de5080c8909
Fai-chebby <chebetf034@gmail.com>
2024-04-14 23:42:38 +0300

    Aws

--- AWS-demo/src/main/java/demo/example/awsdemo/Config/AmazonConfig.java
+++ AWS-demo/src/main/java/demo/example/awsdemo/Config/AmazonConfig.java
@@ -1,7 +1,15 @@
 package demo.example.awsdemo.Config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
+    public AmazonS3 s3(){
+        AWSCredentials awsCredentials=new BasicAWSCredentials(
+                accesskey:"",
+        );
+    }
 }
